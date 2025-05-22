59289b6c08d6c3372884fb5c1402f972aabc5e49
Dimittri Souza <dimittrisouza@gmail.com>
2025-05-21 21:10:29 -0300

    first commit

---
+++ app/src/main/java/com/aerovistadrones/app/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.aerovistadrones.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+public class LoginController {
+
+	@GetMapping
+	public String login() {
+		return "Success";
+	}
+}
